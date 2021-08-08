32d7664d9025074f30d670a5f4b327d7055a8f37
Bill Beesley <bbeesley@me.com>
2021-08-08 15:14:46 +0100

    fix: ensure an output file is supplied

--- main.py
+++ main.py
@@ -7,57 +7,67 @@ import sys
 import pandas as pd
 
 
+def log_usage():
+    print("main.py -p <path to files> -m <match pattern> -o <output file>")
+
+
 def main(argv):
-   # set some defaults
-   sheet_name = "Sheet1"
-   separator = ","
-   path = "./"
-   match = "*.csv"
-   outputfile = ""
+    # set some defaults
+    sheet_name = "Sheet1"
+    separator = ","
+    path = "./"
+    match = "*.csv"
+    outputfile = ""
 
-   # read command line args
-   try:
-       opts, args = getopt.getopt(
-           argv, "hp:m:o:n:s:", ["path=", "match=", "output=", "name=", "separator="]
-       )
-   except getopt.GetoptError:
-       print("main.py -p <path to files> -m <match pattern> -o <output file>")
-       sys.exit(2)
+    # read command line args
+    try:
+        opts, args = getopt.getopt(
+            argv, "hp:m:o:n:s:", ["path=", "match=", "output=", "name=", "separator="]
+        )
+    except getopt.GetoptError:
+        log_usage()
+        sys.exit(2)
 
-   # over write the defaults with command line args (if set)
-   for opt, arg in opts:
-       if opt == "-h":
-           print("main.py -p <path to files> -m <match pattern> -o <output file>")
-           sys.exit()
-       elif opt in ("-p", "--path"):
-           path = arg
-       elif opt in ("-o", "--output"):
-           outputfile = arg
-       elif opt in ("-m", "--match"):
-           match = arg
-       elif opt in ("-n", "--name"):
-           sheet_name = arg
-       elif opt in ("-s", "--separator"):
-           separator = arg
-   
-   # scan for files in the given path matching the glob
-   all_file_paths = glob.glob(os.path.join(path, match))
+    # over write the defaults with command line args (if set)
+    for opt, arg in opts:
+        if opt == "-h":
+            log_usage()
+            sys.exit()
+        elif opt in ("-p", "--path"):
+            path = arg
+        elif opt in ("-o", "--output"):
+            outputfile = arg
+        elif opt in ("-m", "--match"):
+            match = arg
+        elif opt in ("-n", "--name"):
+            sheet_name = arg
+        elif opt in ("-s", "--separator"):
+            separator = arg
 
-   # declare an array for pandas data frames, then read each csv in as a data frame
-   all_data_frames = []
-   for f in all_file_paths:
-       df = pd.read_csv(f, sep=separator)
-       df["file"] = f.split("/")[-1]
-       all_data_frames.append(df)
-   
-   # combine all the data frames from the array
-   merged_data_frame = pd.concat(all_data_frames, ignore_index=True)
+    # check we were given an output file
+    if outputfile == "":
+        print("missing output file name/path")
+        log_usage()
+        sys.exit(1)
 
-   # log out the results for fun (best to comment this out)
-   # print(merged_data_frame)
+    # scan for files in the given path matching the glob
+    all_file_paths = glob.glob(os.path.join(path, match))
 
-   # write out the concatenated results to an excel file
-   merged_data_frame.to_excel(outputfile, sheet_name=sheet_name)
+    # declare an array for pandas data frames, then read each csv in as a data frame
+    all_data_frames = []
+    for f in all_file_paths:
+        df = pd.read_csv(f, sep=separator)
+        df["file"] = f.split("/")[-1]
+        all_data_frames.append(df)
+
+    # combine all the data frames from the array
+    merged_data_frame = pd.concat(all_data_frames, ignore_index=True)
+
+    # log out the results for fun (best to comment this out)
+    # print(merged_data_frame)
+
+    # write out the concatenated results to an excel file
+    merged_data_frame.to_excel(outputfile, sheet_name=sheet_name)
 
 
 if __name__ == "__main__":
